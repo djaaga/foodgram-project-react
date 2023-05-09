6fbb2ea1b290d7ebfd38eb1a0d899f82035c5ce2
Boris Gurev <djaaga5@gmail.com>
2023-05-09 18:07:23 +0400

    fix importcsv\readme

--- backend/api/management/commands/_importcsv.py
+++ backend/api/management/commands/_importcsv.py
@@ -16,7 +16,8 @@ def import_csv():
     ) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            name, measurement_unit = row
+            name = row['name']
+            measurement_unit = row['measurement_unit']
             Ingredient.objects.get_or_create(
                 name=name, measurement_unit=measurement_unit
             )
